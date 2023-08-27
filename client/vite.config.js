bd68cb50f338cdd166a6fc2feb40c7c6af080187
kapil <khatrikapil987@gmail.com>
2023-08-28 00:55:09 +0530

    backend connected

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://127.0.0.1:4000',
+        target: "https://chat-app-rh5f.onrender.com",
         changeOrigin: true,
         secure: false,
       }

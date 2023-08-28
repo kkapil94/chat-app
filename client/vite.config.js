8b92e27deea04b6bb48b411d3de2868b071e11dc
kapil <khatrikapil987@gmail.com>
2023-08-28 23:35:10 +0530

    cors added

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: "https://chat-app-rh5f.onrender.com",
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: "https://chat-app-rh5f.onrender.com",
+  //       changeOrigin: true,
+  //       secure: false,
+  //     }
+  //   }
+  // }
 })

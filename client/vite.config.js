d2891bd3e2da3b53785064d5f8847769074d6de8
kapil <khatrikapil987@gmail.com>
2023-08-28 23:43:19 +0530

    proxy added

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import http from "https"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: "https://chat-app-rh5f.onrender.com",
-  //       changeOrigin: true,
-  //       secure: false,
-  //     }
-  //   }
-  // }
+  server: {
+    proxy: {
+      '/api': {
+        target: "https://chat-app-rh5f.onrender.com",
+        changeOrigin: true,
+        secure: false,
+        agent: new http.Agent()
+      }
+    }
+  }
 })
